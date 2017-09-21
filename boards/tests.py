f3b8e61c368d04c3b57cb7502230a69e25952249
Sunil <sunil9766@gmail.com>
2017-09-21 18:56:47 +0530

    models and static files

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,14 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 # Create your tests here.
+
+class HomeTests(TestCase):
+	def test_home_view_status_code(self):
+		url = reverse('home')
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+
+	def HomeTests(TestCase):
+		view = resolve('/')
+		self.assertEquals(view.func, home)
