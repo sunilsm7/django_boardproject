153ea0f3c229db87dfc33e60db2202e479298090
Sunil <sunil9766@gmail.com>
2017-09-27 13:31:02 +0530

    started form handling

--- boards/views.py
+++ boards/views.py
@@ -1,9 +1,34 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Board
+from .models import Board, Topic, Post
 # Create your views here.
 
 def home(request):
 	boards = Board.objects.all()
 	return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+	board = get_object_or_404(Board, pk=pk)
+	return render(request, 'topics.html', {'board':board})
+
+def new_topic(request, pk):
+	board = get_object_or_404(Board, pk=pk)
+	if request.method == 'POST':
+		subject = request.POST['subject']
+		message = request.POST['message']
+		user = User.objects.first()
+
+		topic = Topic.objects.create(
+			subject=subject,
+			board=board,
+			starter=user
+			)
+		post = Post.objects.create(
+			message=message,
+			topic=topic,
+			created_by=user
+			)
+		return redirect('board_topics', pk=board.pk)
+	return render(request, 'new_topic.html', {'board':board})
