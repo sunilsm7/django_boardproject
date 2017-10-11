76dfc0bce6c1c29a0260a4c10767438324832c61
Sunil <sunil9766@gmail.com>
2017-10-11 13:10:58 +0530

    edit post

--- boards/views.py
+++ boards/views.py
@@ -3,9 +3,15 @@ from django.contrib.auth.models import User
 from django.db.models import Count
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, UpdateView
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
+
 # Create your views here.
 
 def home(request):
@@ -62,3 +68,29 @@ def reply_topic(request, pk, topic_pk):
 	else:
 		form = PostForm()
 	return render(request, 'reply_topic.html', {'topic':topic, 'form':form})
+
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+	model = Post
+	fields = ('message',)
+	template_name = 'edit_post.html'
+	pk_url_kwarg = 'topic_pk'
+	context_object_name = 'post'
+
+	def get_queryset(self):
+		queryset = super().get_queryset()
+		return queryset.filter(created_by=self.request.user)
+
+	def form_valid(self,form):
+		post = form.save(commit = False)
+		post.updated_by = self.request.user
+		post.updated_at = timezone.now()
+		post.save()
+		return redirect('topic_posts', pk=post.topic.board.pk, topic_pk = post.topic.pk)
+
+
+class BoardListView(ListView):
+	model = Board
+	context_object_name = 'boards'
+	template_name = 'home.html'
