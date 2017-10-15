12351c0d1765fbb6bad1acaef47dea4fc31b96aa
Sunil <sunil9766@gmail.com>
2017-10-15 16:23:28 +0530

    final adjustments

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -31,45 +31,46 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'django.contrib.humanize',
 
-    'widget_tweaks',
-
-    'accounts',
-    'boards',
+	'widget_tweaks',
+	'markdown',
+	'accounts',
+	'boards',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'boardproject.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'templates')],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'boardproject.wsgi.application'
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'boardproject.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	}
 }
 
 
@@ -90,18 +91,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 
@@ -125,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+	os.path.join(BASE_DIR, 'static'),
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- boards/views.py
+++ boards/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, UpdateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 
@@ -20,7 +21,19 @@ def home(request):
 
 def board_topics(request, pk):
 	board = get_object_or_404(Board, pk=pk)
-	topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
+	queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts')-1)
+
+	page = request.GET.get('page', 1)
+	paginator = Paginator(queryset, 20)
+	try:
+		topics = paginator.page(page)
+	except PageNotAnInteger:
+		# fallback to the first page
+		topics = paginator.page(1)
+	except EmptyPage:
+		# probably the user tried to add a page number
+		# in the url, so we fallback to the las page
+		topics = paginator.page(paginator.num_pages)
 	return render(request, 'topics.html', {'board':board, 'topics':topics})
 
 	
@@ -41,7 +54,7 @@ def new_topic(request, pk):
 				topic = topic,
 				created_by = request.user
 				)
-			return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+			return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
 	else:
 		form = NewTopicForm()
 	return render(request, 'new_topic.html', {'board':board, 'form': form})
@@ -64,7 +77,16 @@ def reply_topic(request, pk, topic_pk):
 			post.topic = topic
 			post.created_by = request.user
 			post.save()
-			return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+
+			topic.last_updated = timezone.now()
+			topic.save()
+			topic_url = reverse('topic_posts', kwargs={'pk':pk, 'topic_pk': topic_pk})
+			topic_post_url = '{url}?page={page}#{id}'.format(
+				url=topic_url,
+				id=post.pk,
+				page=topic.get_page_count()
+				)
+			return redirect(topic_post_url)
 	else:
 		form = PostForm()
 	return render(request, 'reply_topic.html', {'topic':topic, 'form':form})
@@ -94,3 +116,40 @@ class BoardListView(ListView):
 	model = Board
 	context_object_name = 'boards'
 	template_name = 'home.html'
+
+
+class TopicListView(ListView):
+	model = Topic
+	context_object_name = 'topics'
+	template_name = 'topics.html'
+	paginate_by = 20
+
+	def get_context_data(self, **kwargs):
+		kwargs['board'] = self.board
+		return super().get_context_data(**kwargs)
+
+	def get_queryset(self):
+		self.board = get_object_or_404(Board, pk=self.kwargs.get('pk'))
+		queryset = self.board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+		return queryset
+
+
+class PostListView(ListView):
+	model = Post
+	context_object_name = 'posts'
+	template_name = 'topic_posts.html'
+	paginate_by = 5
+
+	def get_context_data(self, **kwargs):
+		session_key = 'viewed_topic_{}'.format(self.topic.pk)
+		if not self.request.session.get(session_key, False):
+			self.topic.views += 1
+			self.topic.save()
+			self.request.session[session_key] = True
+		kwargs['topic'] = self.topic
+		return super().get_context_data(**kwargs)
+
+	def get_queryset(self):
+		self.topic = get_object_or_404(	Topic, board__pk = self.kwargs.get('pk'), pk=self.kwargs.get('topic_pk'))
+		queryset = self.topic.posts.order_by('created_at')
+		return queryset
